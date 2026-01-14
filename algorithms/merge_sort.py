5886fe15581e16d6cc2337b9c7ff3e7805083dc6
Aziz Mansaray <aamansaray2@gmail.com>
2026-01-14 13:32:54 -0500

    merge sort

--- algorithms/merge_sort.py
+++ algorithms/merge_sort.py
@@ -1,58 +1,21 @@
-def merge(arr, left, mid, right):
-    n1 = mid - left + 1
-    n2 = right - mid
+def merge(left_arr, right_arr):
+    if len(left_arr) == 0:
+        return right_arr
+    if len(right_arr) == 0:
+        return left_arr
+    if left_arr[0] < right_arr[0]:
+        return [left_arr[0]] + merge(left_arr[1:], right_arr)
+    if right_arr[0] < left_arr[0]:
+        return [right_arr[0]] + merge(left_arr, right_arr[1:])
 
-    # Create temp arrays
-    L = [0] * n1
-    R = [0] * n2
+def mergeSort(arr):
+    if len(arr) == 1:
+        return arr
+    middle = len(arr)//2
+    left_arr = mergeSort(arr[:middle])
+    right_arr = mergeSort(arr[middle:])
+    return merge(left_arr, right_arr)
 
-    # Copy data to temp arrays L[] and R[]
-    for i in range(n1):
-        L[i] = arr[left + i]
-    for j in range(n2):
-        R[j] = arr[mid + 1 + j]
-        
-    i = 0  
-    j = 0  
-    k = left  
-
-    # Merge the temp arrays back
-    # into arr[left..right]
-    while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            arr[k] = L[i]
-            i += 1
-        else:
-            arr[k] = R[j]
-            j += 1
-        k += 1
-
-    # Copy the remaining elements of L[],
-    # if there are any
-    while i < n1:
-        arr[k] = L[i]
-        i += 1
-        k += 1
-
-    # Copy the remaining elements of R[], 
-    # if there are any
-    while j < n2:
-        arr[k] = R[j]
-        j += 1
-        k += 1
-
-def mergeSort(arr, left, right):
-    if left < right:
-        mid = (left + right) // 2
-        mergeSort(arr, left, mid)
-        mergeSort(arr, mid + 1, right)
-        merge(arr, left, mid, right)
-
-# Driver code
-if __name__ == "__main__":
-    arr = [38, 27, 43, 10]
-   
-    mergeSort(arr, 0, len(arr) - 1)
-    for i in arr:
-        print(i, end=" ")
-    print()
+arr = [38, 27, 43, 10]
+assert mergeSort(arr) == [10, 27, 38, 43]
+print(arr)
